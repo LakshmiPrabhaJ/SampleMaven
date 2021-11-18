d9b7a57ee1b84f8437950f74ca4b288851d8c4eb
LakshmiPrabhaJ <lakshmijkumar79@gmail.com>
2021-11-17 22:32:19 -0800

    first commit

---
+++ src/test/java/mavensample/FirstTest.java
@@ -0,0 +1,19 @@
+package mavensample;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	@Test 
+	public void test1() {
+		System.out.println("First Test");
+	}
+	@Test 
+	public void test2() {
+		System.out.println("Second Test");
+	}
+	@Test 
+	public void test3() {
+		System.out.println("Third Test");
+	}
+
+}
